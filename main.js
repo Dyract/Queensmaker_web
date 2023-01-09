dc34014bd20be2f09893b4bfc80868c05573ab74
pma <pma@pmaui-MacBookPro.local>
2023-01-09 17:08:40 +0900

    게임오버화면 구현

---
+++ main.js
@@ -0,0 +1,14 @@
+// 게임오버 창 닫기
+  function closeGameover() {
+    console.log("버튼")
+    $('#fade').hide();
+    $('#light').hide();
+}
+
+// 게임오버 창 띄우기
+function showGameover() {
+    console.log("버튼")
+    $('#fade').show();
+    $('#light').show();
+}
+
